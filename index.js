7364e9ab079ae665728dfcf66b46eead85111b50
Tim Severien <timseverien@gmail.com>
2014-11-25 16:10:27 +0100

    1.0.1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var pad = function(input, length, padding, side) {
+    input = (input || '').toString();
 	length = parseInt(length) || 0;
 	padding = (padding || '').toString() || ' ';
 	side = parseInt(side) || pad.LEFT;
